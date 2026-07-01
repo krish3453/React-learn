e37b28739e4adf4a0a387e5dd1ef7f4d24acc1fc
krish3453 <krishvij1234@gmail.com>
2026-07-02 03:41:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function App() {
+  return (
+    <div>helllo</div>
+  )
+}
+
+let ele=App()
+console.log(ele)
+
+export default App
---
+++ src/Compo.jsx
@@ -0,0 +1,7 @@
+function Compo() {
+     let a=10;
+     let b=12;
+     console.log(a+b)
+     return a+b
+}
+export default Compo
